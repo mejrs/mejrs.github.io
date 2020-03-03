cdfae10d7c82b599d6bd3dedb56c6b76b316f88b
mejrs <brunokolenbrander@hotmail.com>
2020-03-03 09:15:19 +0100

    osrs

---
+++ js/main_osrs.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var runescape_map = L.gameMap('map', {
+        crs: L.CRS.Simple,
+        maxBounds: [[-1000, -1000], [12800 + 1000, 6400 + 1000]],
+        maxBoundsViscosity: 0.5,
+        zoomControl: false,
+        fullscreenControl: true,
+		zoom: 2,
+        plane: 0,
+        x: 3232,
+        y: 3232,
+        minPlane: 0,
+        maxPlane: 3,
+        doubleClickZoom: false,
+        iconMode: "",
+        showMapBorder: true,
+        enableUrlLocation: true
+    });
+	
+var main = L.tileLayer.main('layers/{source}/{zoom}/{plane}_{x}_{y}.png', {
+        source: 'map_squares_osrs',
+        minZoom: -4,
+        maxNativeZoom: 3,
+        maxZoom: 4,
+		 attribution: '<a href="https://explv.github.io/">Tiles @ Explv</a>',
+
+    }).addTo(runescape_map);
+
+L.control.mapTemplate().addTo(runescape_map);
+L.control.plane().addTo(runescape_map);
+L.control.mousePosition().addTo(runescape_map);
+L.control.customZoom().addTo(runescape_map);
