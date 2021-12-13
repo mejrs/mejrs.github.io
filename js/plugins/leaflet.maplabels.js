6ddb9abea76d5508e9030ec72269763a40d74363
mejrs <brunokolenbrander@hotmail.com>
2021-12-13 15:04:39 +0100

    format some things

--- js/plugins/leaflet.maplabels.js
+++ js/plugins/leaflet.maplabels.js
@@ -1,60 +1,59 @@
 import "../leaflet.js";
 
-export default void function (factory) {
+export default void (function (factory) {
     var L;
     if (typeof define === "function" && define.amd) {
-        define(["leaflet"], factory)
+        define(["leaflet"], factory);
     } else if (typeof module !== "undefined") {
         L = require("leaflet");
-        module.exports = factory(L)
+        module.exports = factory(L);
     } else {
         if (typeof window.L === "undefined") {
-            throw new Error("Leaflet must be loaded first")
+            throw new Error("Leaflet must be loaded first");
         }
-        factory(window.L)
+        factory(window.L);
     }
-}
-(function (L) {
+})(function (L) {
     let MaplabelGroup = L.LayerGroup.extend({
         initialize: function (options) {
             L.LayerGroup.prototype.initialize.call(this, {}, options);
             let url = `https://sheets.googleapis.com/v4/spreadsheets/${this.options.SHEET_ID}/values/A:Z?key=${this.options.API_KEY}`;
-            fetch(url).then(res => res.json()).then(sheet => {
-                
-                let markers = this.parse_sheet(sheet);
-                let marker_iter =  markers[Symbol.iterator]();
-                for (const marker of marker_iter){
-                    this.addLayer(marker);
-                }
-            })
+            fetch(url)
+                .then((res) => res.json())
+                .then((sheet) => {
+                    let markers = this.parse_sheet(sheet);
+                    let marker_iter = markers[Symbol.iterator]();
+                    for (const marker of marker_iter) {
+                        this.addLayer(marker);
+                    }
+                });
         },
 
-        parse_sheet: function(sheet){
-            return sheet.values.map(row => this.create_textlabel(...row))
+        parse_sheet: function (sheet) {
+            return sheet.values.map((row) => this.create_textlabel(...row));
         },
 
-        create_textlabel: function (x,y,plane, description){
-            
+        create_textlabel: function (x, y, plane, description) {
             let text = document.createTextNode(description);
-            let html = document.createElement('div');
+            let html = document.createElement("div");
             html.appendChild(text);
 
             let divicon = L.divIcon({
                 html: html,
                 iconSize: null, // I love gross hacks! necessary to not make the text 12x12px
-                className: 'map-label'
+                className: "map-label",
             });
 
             let marker = L.marker([Number(y), Number(x)], {
                 icon: divicon,
-                className: 'map-label-container'
+                className: "map-label-container",
             });
 
             return marker;
-        }
+        },
     });
 
     L.maplabelGroup = function (options) {
         return new MaplabelGroup(options);
-    }    
+    };
 });
